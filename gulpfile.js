1d8787ce1c55b0061e7bc555568de05893721dd6
Daniel Fernandes <daniel.fernandes@namics.com>
2016-04-20 16:49:11 +0200

    added less task to gulp and package in .json

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,51 @@
-var gulp           = require('gulp'),
-    typescript    = require('gulp-typescript'),
-    browserSync         = require('browser-sync'),
-    historyApiFallback  = require('connect-history-api-fallback'),
-    reload              = browserSync.reload
-    ;
+var gulp = require('gulp'),
+	typescript = require('gulp-typescript'),
+	browserSync = require('browser-sync'),
+	historyApiFallback = require('connect-history-api-fallback'),
+	reload = browserSync.reload,
+	less = require('gulp-less'),
+	path = require('path')
+	;
 
 
-tsFiles =  [
-    'typeScripts/*.ts'
+tsFiles = [
+	'typeScripts/*.ts'
 ];
 
+
+
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
 gulp.task('compileTypeScript', function () {
-    return gulp.src(tsFiles)
-        .pipe(typescript({
-            noImplicitAny: true,
-            out: 'output.js'
-        }))
-        .pipe(gulp.dest('scripts'))
-        .pipe(reload({stream:true}));
+	return gulp.src(tsFiles)
+		.pipe(typescript({
+			noImplicitAny: true,
+			out: 'output.js'
+		}))
+		.pipe(gulp.dest('scripts'))
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('typeScripts/*.ts', ['compileTypeScript']);
-    gulp.watch('*.html', ['compileTypeScript']);
+	gulp.watch('typeScripts/*.ts', ['compileTypeScript']);
+	gulp.watch('*.html', ['compileTypeScript']);
 
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server : {},
-        middleware : [ historyApiFallback() ],
-        ghostMode: false
-    });
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {},
+		middleware: [historyApiFallback()],
+		ghostMode: false
+	});
 });
 
 
-
-
-gulp.task('default', [ 'compileTypeScript', 'watch', 'browser-sync' ]);
+gulp.task('default', ['compileTypeScript', 'watch', 'browser-sync']);
