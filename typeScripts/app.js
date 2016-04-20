499efb793ac2aa2950428e69bf8a1ce09f7d0b8c
Bladimir Ardiles <bladi_sys@hotmail.com>
2016-04-20 17:10:13 +0200

    Cleaning code

--- typeScripts/app.js
+++ typeScripts/app.js
@@ -1,4 +1,9 @@
+var songList = new Playlist(), songOne = new Song('test', 'test', 'test', false), songTwo = new Song('test', 'test', 'test', false), songThree = new Song('test', 'test', 'test', false);
+songList.addSong(songOne);
+songList.addSong(songTwo);
+songList.addSong(songThree);
+var playlistHtml = document.createElement('ul');
+document.body.appendChild(playlistHtml);
+songList.renderInElement(playlistHtml);
 var songList = new Playlist(), song1 = new Song('test title', 'test artist', 'test duration', false);
-console.log(songList);
-console.log(song1);
 //# sourceMappingURL=app.js.map
--- typeScripts/app.ts
+++ typeScripts/app.ts
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
-var songList = new Playlist(),
-    songOne = new Song('test', 'test', 'test', false),
-    songTwo = new Song('test', 'test', 'test', false),
-    songThree = new Song('test', 'test', 'test', false);
 
+var songList    = new Playlist(),
+    songOne     = new Song('test', 'test', 'test', false),
+    songTwo     = new Song('test', 'test', 'test', false),
+    songThree   = new Song('test', 'test', 'test', false)
+;
 
 songList.addSong(songOne);
 songList.addSong(songTwo);
 songList.addSong(songThree);
-
-var playlist = document.getElementsByClassName('playlist')[0];
 
 var playlistHtml = document.createElement('ul');
 
@@ -17,14 +15,9 @@ document.body.appendChild(playlistHtml);
 
 songList.renderInElement(playlistHtml);
 
-=======
-
-
-
 var songList = new Playlist(),
     song1 = new Song('test title', 'test artist', 'test duration', false)
 ;
 
-console.log(songList);
-console.log(song1);
->>>>>>> 1fb1c075787d1ba9127cdeaea99076bf834b47b9
+
+
